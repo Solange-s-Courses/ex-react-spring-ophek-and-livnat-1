46810bc80050ab1b5c5007fe4cbca0d46069aef9
Ophek Alon <ophekal@std.hac.ac.il>
2025-05-06 14:00:51 +0300

    start project

---
+++ src/main/java/com/example/backendex3/BackendEx3Application.java
@@ -0,0 +1,13 @@
+package com.example.backendex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEx3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEx3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendex3/BackendEx3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEx3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
