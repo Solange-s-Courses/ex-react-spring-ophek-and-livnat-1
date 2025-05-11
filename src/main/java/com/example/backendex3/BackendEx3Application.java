ee64a3391dcbd64856eb7dd8f24efa1c60661003
Livnat Arama <livnatar@std.hac.ac.il>
2025-05-11 19:26:07 +0300

    update leaderboard to make this page to work

--- src/main/java/com/example/backendex3/BackendEx3Application.java
+++ src/main/java/com/example/backendex3/BackendEx3Application.java
@@ -3,7 +3,10 @@ package com.example.backendex3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.backendex3", "controllers", "services", "repositories", "dto","utils"})
 public class BackendEx3Application {
 
     public static void main(String[] args) {
